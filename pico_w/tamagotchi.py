829987d14b441cfa9ff23fbe24d2ed62cd9a4be5
mariaapenya <mariaastolfi2005@gmail.com>
2026-06-14 13:53:46 +0200

    Update tamagotchi.py

--- pico_w/tamagotchi.py
+++ pico_w/tamagotchi.py
@@ -1,1 +1,13 @@
+def obtener_estado(humedad):
 
+    if humedad < 10:
+        return "muerta"
+
+    elif humedad < 30:
+        return "triste"
+
+    elif humedad < 70:
+        return "normal"
+
+    else:
+        return "feliz"
