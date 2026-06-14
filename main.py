8ced66578cc8e14864b86f4984188c3e27139e3d
mariaapenya <mariaastolfi2005@gmail.com>
2026-06-14 13:54:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from sensores import leer_humedad
+from tamagotchi import obtener_estado
+from pantalla import mostrar_estado
 
+while True:
+
+    humedad = leer_humedad()
+
+    estado = obtener_estado(humedad)
+
+    mostrar_estado(estado)
