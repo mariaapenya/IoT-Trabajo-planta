16838520a61294ec652923e2bc1231ed8c205cac
mariaapenya <mariaastolfi2005@gmail.com>
2026-06-16 11:49:47 +0200

    Update main.py

--- pico_w/main.py
+++ pico_w/main.py
@@ -1,11 +1,47 @@
-from sensores import leer_humedad
-from tamagotchi import obtener_estado
-from pantalla import mostrar_estado
+import time
+import ujson
+from umqtt.simple 
+import MQTTClient
+from sensores 
+import leer_humedad_pct
+from oled_display 
+import OledDisplay
+import config
+
+#iniciar la pantalla (cuidado porque es otra)
+oled   = OledDisplay()
+
+#iniciar cliente MQTT
+cliente = MQTTClient(config.MQTT_CLIENT, config.MQTT_BROKER, config.MQTT_PORT)
+def conectar_mqtt():
+    try:
+        cliente.connect()
+        print('MQTT OK ->', config.MQTT_BROKER)
+        return True
+    except Exception as e:
+        print('MQTT error:', e); 
+        return False
+conectar_mqtt()
 
 while True:
+    #lee la humedad del suelo
+    humedad = leer_humedad_pct()
+    #mensaje JSON no es necesario en verdad pero simplifica mucho y para futuras ampliaciones
+    payload = ujson.dumps({
+        "humedad": humedad
+    })
 
-    humedad = leer_humedad()
+    #publicar datos por MQTT
+    try:
+        cliente.publish(config.MQTT_TOPIC, payload)
+        print("Publicado:", payload)
 
-    estado = obtener_estado(humedad)
+    except Exception as e:
+        print("Error publicando MQTT:", e)
+        conectar_mqtt()
 
-    mostrar_estado(estado)
+    #actualizar Tamagotchi en pantalla con la función en oled_display.py
+    oled.actualizar(humedad)
+
+    # Esperar hasta la siguiente lectura
+    time.sleep(config.INTERVALO_S)
